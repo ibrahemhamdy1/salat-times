be692506eec81e60c5819e242e34fa45ce899822
ibrahim hamdy <ihamdy@boca.pro>
2019-02-21 14:13:44 +0200

    Add users test and functionality.

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+
+    /** @test */
+   public function create_new_user()
+   {
+      $this->assertDatabaseHas('users', $this->createUser()->toArray());
+   }
+
+
+    /** @test */
+    public function new_user_can_login()
+    {
+       $user = $this->createUser();
+
+       $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret',
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertAuthenticatedAs($user);
+    }
+
+
+    /** @test */
+    public function invalid_user_can_not_login()
+    {
+        $response = $this->post('/login', [
+            'email' => $this->createUser()->email,
+            'password' => 'invalid'
+        ]);
+
+        $response->assertSessionHasErrors();
+
+        $this->assertGuest();
+    }
+
+    /** @test */
+    public function auth_user_can_logout()
+    {
+        $user = $this->logInUser();
+
+        $this->assertAuthenticatedAs($user);
+
+        $response = $this->actingAs($user)->post('/logout');
+
+        $response->assertStatus(302);
+
+        $this->assertGuest();
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,35 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    /**
+     * Create new user
+     *
+     * @return \App\User;
+     */
+    public function createUser(){
+        return factory('App\User')->create();
+    }
+
+    /**
+     * Login new user
+     *
+     * @return \App\User;
+     */
+    public function logInUser()
+    {
+        $user = $this->createUser();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret'
+        ]);
+
+        return $user;
+    }
 }
