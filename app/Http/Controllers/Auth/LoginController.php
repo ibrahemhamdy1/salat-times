5b8f90f874a0f546440edb1d8849b3ba590f2135
ibrahim hamdy <ihamdy@boca.pro>
2019-03-19 14:39:57 +0200

    Update the redirectTo after login and registeration.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/masjids';
 
     /**
      * Create a new controller instance.
