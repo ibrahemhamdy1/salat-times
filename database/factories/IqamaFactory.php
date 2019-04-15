2bc27898913170107febf5d5974d777c506dd257
Ibrahim Hamdy <ibrahimhamdy@Ibrahims-MacBook-Pro.local>
2019-04-15 15:27:58 +0200

    Update iqama factory.

--- database/factories/IqamaFactory.php
+++ database/factories/IqamaFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Iqama::class, function (Faker $faker, $attributes) {
     return [
-        'date' => $faker->date,
+        'date' => $faker->dateTimeBetween('-10 years', 'now')->format("Y-m-d"),
         'fajr' => $faker->time,
         'duhr' => $faker->time,
         'asr' => $faker->time,
