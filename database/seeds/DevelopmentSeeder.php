5116086cff6a0ae7132ab397605f1f41a9156f4f
Ibrahim Hamdy <ibrahimhamdy@Ibrahims-MacBook-Pro.local>
2019-04-15 15:27:27 +0200

    Update the development seeder.

--- database/seeds/DevelopmentSeeder.php
+++ database/seeds/DevelopmentSeeder.php
@@ -2,6 +2,8 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use App\Masjid;
+use App\Iqama;
 
 class DevelopmentSeeder extends Seeder
 {
@@ -18,6 +20,23 @@ class DevelopmentSeeder extends Seeder
             'password' => bcrypt('test123'),
         ];
 
-        User::updateOrCreate(array_only($userData, 'email'), array_except($userData, 'email'));
+        $user = User::updateOrCreate(array_only($userData, 'email'), array_except($userData, 'email'));
+
+        $masjidData = [
+            'name' => 'denton mosque',
+            'address_1' => '1105 Greenlee St, Denton, TX 76201',
+            'phone' => '940-484-1871',
+            'lat' => '33.201370',
+            'lng' => '-97.144830',
+            'country_id' => 1,
+            'calculation_method_id' => 1,
+            'juristic_setting_id' => 1,
+        ];
+
+        $masjid = Masjid::updateOrCreate(array_only($masjidData, 'name'), array_except($masjidData, 'name'))->users()->attach($user);
+
+        factory(Iqama::class, 5)->create([
+            'masjid_id' => Masjid::first()->id
+        ]);
     }
 }
